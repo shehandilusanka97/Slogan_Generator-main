344bd62669f34aba37ea6497e19951b8cfdac256
Shehan Dilusanka <72766858+shehandilusanka97@users.noreply.github.com>
2022-07-09 04:08:02 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,14 +1,5 @@
 export const dialogflowConfig = {
-    "type": "service_account",
-    "project_id": "chatbot-ltgp",
-    "private_key_id": "8c3e56e73dcc8bbd10deb7bf6badec5e77e2be57",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDpBUJUMr8gyepT\naVKDc//Hzj57KHxJu3hDPv+7JJju2Z9GJtPSD4svKBkDcw5spBsnfftJCvyllygE\nKwT5wbW655j3fbKZTv8VVs3zpfyDHkCF2gXEKl0014Nn4A+pl+ECCE8hfezRIBjh\nD4xLZEFkjTVOFT+GCASk2nz+FLm4j/iNdIFf7k7Jqy8nG6ac6Dud5bQiXBIMnCqd\nrcn9fh9M9+sO6jTbV0BbLWxGPaZCkDQ6Jntt2lYW71oEtsV20nsdgdXnpsm3Q79y\nO7xe1n4ChK7FSVWdFjT/CeVSKSiLmvdepps5vM/8F5tMPQFrYzlWrYwUDjphIGLe\nekHFguDBAgMBAAECggEAXyWOl+LAuBq6W6S91H8hCezlBlTZqyGpUYv/z+J5r82c\ng4CHLVa7dl3hGM96LFI15EJEwOaDkvnNlSYbuf+mvNKkWyowKvlens/6+GnmWYOY\nxQ7pG9hQhqsZNOkAfDdP4QyTgxFwCVQZ1IwMtRwSt4QRJ/j1Hz1W6BluwDCmRP18\n/FwQwiGN+2mx7ZPzTiFexS6oGWbBDX5BJVxy9TkFTFj4cMF1t7wD5iaX5//E52Kk\neQOj7AWNNfJslpe0Ne7dNOptfZOBlPBG+UOmj0WAbGQRIqcG4wBA+EYeBcOtfyHO\nnd8nBvgRmJKjt1dKxCfFu+P9h4PaCWy3zOyXL74UoQKBgQD4E5+kaSXncvJYY6u0\nXr3DYD9Zj8dhiziypo/XjXu/KrnefSj9RclmTBjGfNY4XcmuKKPmND6wmVf1ZXcG\nWUnaXCYGA4THdANr7IfcKpRlN/nFq9AgFIBenEO5dRJA/YYbvDU6M/Jnhl/bvUgd\nNipzyKkxX6pgzvnY8I3GbhvXOwKBgQDwdofSpj+3WAaykhgny6EMMPhYgXLWuxm0\nZB8rIcFgKLAJQw/A8q1ZKHcpvdnMRA/1KHf5aKlB44kiQkxFYBZV3udOoNoACfzJ\nmLvp0bj0RGpU3nc0XbxLjS6APc9Q1hrZV3Z1tErmbPPci493WKHBdqiAjfUraOlg\nvUip6HUAMwKBgQC7JRKp1SjjEbQBOi+uQkqluVYRGoYu7/g1zbF1yyYvcOwHDCR/\n7MWihoWqN0LkAtzCpaX/ZCpmz0eruH+yKmmscRe+MalZTgvGvwk+7mBEe5XRqWu4\nx76PoRL2NXsvzBH/mlwNLGMR9i0aeBCO+O3NqvL4bFL79ShGoDJUH3yhQQKBgQCJ\nJ8KfB06OgDoJvsTiZgZtEr2Elglvq3ak6vrQOke3Vl3aWpLwxjrUAP2/JFfHaAcG\nGdgcvXLVbzg3/0LLy1vSDPs4YQ0XOpsiTejZHFN3mFJiKCYsC08sdNHSOe83N3hH\nAdlCl9ylA/4wjLX/J+HTnJ/N+6qc4RFEqZJ5v2qE+wKBgGUx1yEFDsJZgzxEToCk\nhEFDJjkneahnZLYFevh3iKFQxJSHvr2R3M6RjmCnLKuLo7lBNnDQVcNknqmyNcpa\nIzQdQkbmmj7X//abLIvitGrWg3b8aCBuKBRaPBP1KayJtKFWenbAH09qt9BnG77u\nVS45fUj2C8RMHOQIeUN4gdWV\n-----END PRIVATE KEY-----\n",
-    "client_email": "sloganchatbot@chatbot-ltgp.iam.gserviceaccount.com",
-    "client_id": "104896196325036968011",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/sloganchatbot%40chatbot-ltgp.iam.gserviceaccount.com"
+   ????
   }
   
 
