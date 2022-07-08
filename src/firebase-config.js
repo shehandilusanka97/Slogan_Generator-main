9628f47cb336f6a60b0acb6fffb4a4c7a03e5c63
Shehan Dilusanka <72766858+shehandilusanka97@users.noreply.github.com>
2022-07-09 02:16:39 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBG_yibsshTEudftgranYKYbTUr5tJSmec",
-    authDomain: "slogan-generator-7cfca.firebaseapp.com",
-    projectId: "slogan-generator-7cfca",
-    storageBucket: "slogan-generator-7cfca.appspot.com",
-    messagingSenderId: "103878545217",
-    appId: "1:103878545217:web:f54a01b73bfecfa4149bc0"
+   ////////
   };
   const app = initializeApp(firebaseConfig);
   export const db=getFirestore(app);
